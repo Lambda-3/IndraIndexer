dee50ecd943e5daa9b72cc21129b057205c9c893
Leonardo Souza <lsouza@amtera.com.br>
2017-04-27 15:08:29 +0200

    Initial code.

---
+++ src/main/java/org/lambda3/indra/loader/IndraLoader.java
@@ -0,0 +1,10 @@
+package org.lambda3.indra.loader;
+
+/**
+ * Main Entry point of all loaders/importers/generators.
+ */
+public final class IndraLoader {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
