e0257c1a6984ae896dc8b3af77a0b7bc3b67666c
Juliano Efson Sales <julianoens@gmail.com>
2017-11-07 18:35:17 +0100

    refactoring and prepering for multiple loaders

--- indra-indexer/src/main/java/org/lambda3/indra/loader/LoaderCommandLine.java
+++ indra-indexer/src/main/java/org/lambda3/indra/loader/LoaderCommandLine.java
@@ -1,4 +1,7 @@
 package org.lambda3.indra.loader;
 
 public class LoaderCommandLine {
+
+    private static final String INDEXER_NAME = "Indra Indexer v.%s";
+
 }
---
+++ lucene-loader/src/main/java/org/lambda3/data/loader/LuceneIndraLoader.java
@@ -0,0 +1,12 @@
+package org.lambda3.data.loader;
+
+import org.lambda3.indra.loader.IndraLoader;
+import org.lambda3.indra.loader.RawSpaceModel;
+
+public class LuceneIndraLoader implements IndraLoader {
+
+    @Override
+    public void load(RawSpaceModel rsm) {
+        //TODO implement me.
+    }
+}
