5a7ed899a9ce28e0f425b83e2a71dfe29522850b
Juliano Efson Sales <julianoens@gmail.com>
2018-04-24 18:06:48 +0200

    adding xmx

--- shellindra.sh
+++ shellindra.sh
@@ -17,19 +17,19 @@ if [ "$COMMAND" == "$CONFIG" ]; then
 fi
 
 if [ "$COMMAND" == "$PREPROCESS" ]; then
-   java -cp indra-preprocessing/target/indra-preprocessing-1.1.0-rc3-jar-with-dependencies.jar org.lambda3.indra.pp.IndraPreProcessorCommandLine pp "${@:2}"
+   java -cp -Xmx20g indra-preprocessing/target/indra-preprocessing-1.1.0-rc3-jar-with-dependencies.jar org.lambda3.indra.pp.IndraPreProcessorCommandLine pp "${@:2}"
 fi
 
 if [ "$COMMAND" == "$INDEX" ]; then
-   java -cp indra-index/target/indra-index-1.1.0-rc3-jar-with-dependencies.jar org.lambda3.indra.indexer.IndraIndexerCommandLine index "${@:2}"
+   java -cp -Xmx20g indra-index/target/indra-index-1.1.0-rc3-jar-with-dependencies.jar org.lambda3.indra.indexer.IndraIndexerCommandLine index "${@:2}"
 fi
 
 if [ "$COMMAND" == "$LOAD" ]; then
-   java -cp indra-index/target/indra-index-1.1.0-rc3-jar-with-dependencies.jar org.lambda3.indra.loader.LoaderCommandLine load "${@:2}"
+   java -cp -Xmx20g indra-index/target/indra-index-1.1.0-rc3-jar-with-dependencies.jar org.lambda3.indra.loader.LoaderCommandLine load "${@:2}"
 fi
 
 if [ "$COMMAND" == "$TEST" ]; then
-   java -cp indra-index/target/indra-index-1.1.0-rc3-jar-with-dependencies.jar org.lambda3.indra.loader.LoaderCommandLine test "${@:2}"
+   java -cp -Xmx20g indra-index/target/indra-index-1.1.0-rc3-jar-with-dependencies.jar org.lambda3.indra.loader.LoaderCommandLine test "${@:2}"
 fi
 
 
